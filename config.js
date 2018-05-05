fdfaf8d3d4571da9ae7f97c3c700568fc4bf9738
Camille Wescott <35951309+Camgerine@users.noreply.github.com>
2018-05-05 14:29:46 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+//config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+module.exports = {
+  consumer_key: 'TPTnszK927AwznROpcBFiLvww',  
+  consumer_secret: 'ETA7MdWI7Ub4IjUozmQLtJFRe39Q3ZFBtF1xG7Ns8yMpa0WrAd',
+  access_token: '983115791586357251-cBwzYkhO0ycXbJSPoXofJYAgN0Oqlyl',  
+  access_token_secret: 'AKoER4P2PxQPxs3YoAg1S53dAxmWtoCdxLWJmFiYmaNNA'
+}
